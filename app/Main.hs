2746f106979b104ff0319f7f30155f2c9e8885e4
Borisov Sergey <b.f.g.86@mail.ru>
2025-01-11 16:14:26 +0500

    Add test code with using ncurses

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,45 @@
 module Main (main) where
 
-import Lib
-import Control.Concurrent
+--import Lib()
+--import Control.Concurrent
+import UI.NCurses
+
+-- main :: IO ()
+-- main = do
+--     putStrLn "Hello World"
+--     threadDelay oneSec
+--     main
+--     where
+--         oneSec = 1000000
+
+
+
+--showCurses :: Curses (Integer, Integer) -> String
+--showCurses y  = show y
 
 main :: IO ()
-main = do
-    putStrLn "Hello World"
-    threadDelay oneSec
-    main
+main = runCurses $ do
+    setEcho False
+
+    --print (showCurses size1)
+
+    w <- defaultWindow
+    updateWindow w $ do
+        moveCursor 1 10
+        drawString "Hello world!"
+        moveCursor 3 10
+        drawString "(press q to quit)"
+        moveCursor 0 0
+    render
+    waitFor w (\ev -> ev == EventCharacter 'q' || ev == EventCharacter 'Q')
     where
-        oneSec = 1000000
+        size1 = screenSize
+
+waitFor :: Window -> (Event -> Bool) -> Curses ()
+waitFor w p = loop where
+    loop = do
+        ev <- getEvent w Nothing
+        case ev of
+            Nothing -> loop
+            Just ev' -> if p ev' then return () else loop
 
