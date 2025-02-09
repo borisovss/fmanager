ce621a7e7cd1fee348d6a00db8d8b7217daa70bb
Borisov Sergey <b.f.g.86@mail.ru>
2025-02-09 20:41:41 +0500

    Add displaying content of directories, remove unused

--- app/Main.hs
+++ app/Main.hs
@@ -1,93 +1,51 @@
 module Main (main) where
 
---import Lib()
---import Control.Concurrent
+import Control.Monad
+import Control.Monad.IO.Class
 import UI.NCurses
-
--- main :: IO ()
--- main = do
---     putStrLn "Hello World"
---     threadDelay oneSec
---     main
---     where
---         oneSec = 1000000
-
-
-
---showCurses :: Curses (Integer, Integer) -> IO String
---showCurses x = show x
-
--- showCurses :: (Integer, Integer) -> IO ()
--- showCurses (x, y) = do
---     --(x,y) <- v
---     -- putStrLn . show $ x
---     putStrLn "A"
+import System.Directory
+import Data.List
 
 main :: IO ()
-main = runCurses $ createCurses
-
-
--- main :: IO ()
--- main = runCurses $ do
---     setEcho False
-
---     (maxRows, maxCols) <- screenSize
---     --print "Hello"
-
---     w <- defaultWindow
---     updateWindow w $ do
---         moveCursor 1 10
---         drawString "Hello world!"
---         moveCursor 3 10
---         drawString "(press q to quit)"
---         moveCursor 0 0
---     render
---     waitFor w (\ev -> ev == EventCharacter 'q' || ev == EventCharacter 'Q')
+main = do
+    runCurses $ createCurses
 
 waitFor :: Window -> (Event -> Bool) -> Curses ()
-waitFor w p = loop where
+waitFor win p = loop
+    where
     loop = do
-        ev <- getEvent w Nothing
+        ev <- getEvent win Nothing
         case ev of
             Nothing -> loop
             Just ev' -> if p ev' then return () else loop
+
+createUpdate :: [String] -> Integer -> Update ()
+createUpdate files row
+    | length files /= 0 = do
+        drawBox Nothing Nothing
+        moveCursor row 1
+        drawString $ head files
+        createUpdate (tail files) (row + 1)
+    | otherwise = do
+        drawBox Nothing Nothing
 
 createCurses :: Curses ()
 createCurses = do
     setEcho False
 
-    --w <- defaultWindow
-
-    --maxx <- getmaxx w
+    rootFiles <- liftIO $ getDirectoryContents "/"
+    localFiles <- liftIO $ getDirectoryContents "./"
 
     (maxRows, maxCols) <- screenSize
     win1 <- newWindow maxRows (maxCols `div` 2) 0 0
-    win2 <- newWindow maxRows (maxCols `div` 2) 0 (maxCols `div` 2) 
+    win2 <- newWindow maxRows (maxCols `div` 2) 0 (maxCols `div` 2)
 
-    updateWindow win1 $ do
-        drawBox Nothing Nothing
-        moveCursor 1 1
-        drawString "Green"
-        moveCursor 2 1
-        drawString "Red"
-        moveCursor 3 1
-        drawString "Blue"
-        moveCursor 4 1
-        drawString "Black"
+    let sortedRootFiles = sort rootFiles
+    let sortedLocalFiles = sort localFiles
+    let startedRow = 1
 
-    updateWindow win2 $ do
-        drawBox Nothing Nothing
-        moveCursor 1 1
-        drawString "Yellow"
-        moveCursor 2 1
-        drawString "Brown"
+    updateWindow win1 (createUpdate sortedRootFiles startedRow) 
+    updateWindow win2 (createUpdate sortedLocalFiles startedRow) 
 
-    -- w <- defaultWindow
-    -- updateWindow w $ do
-    --     moveCursor 1 10
-    --     drawString "Hello world!"
-    --     moveCursor 3 10
-    --     drawString "(press q to quit)"
-    --     moveCursor 0 0
     render
     waitFor win1 (\ev -> ev == EventCharacter 'q' || ev == EventCharacter 'Q')
